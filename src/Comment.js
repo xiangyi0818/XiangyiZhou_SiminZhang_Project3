d322427b820bafd44af3c69371854b3e79ca660c
simin-zhang <siminzhang94@gmail.com>
2021-05-01 14:38:30 -0700

    test

--- src/Comment.js
+++ src/Comment.js
@@ -71,17 +71,21 @@ export default class Comment extends React.Component {
 
         return(
             <div>
-            <div>
-            creator: {this.props.username},
-            creationTime: {this.props.creationTime}
-            </div>
-            <div>
-            content: {this.props.content}
-            </div>
-            {/* <input type="text" value={this.state.content} onChange={e => this.setState({content: e.target.value})}></input> */}
-            {this.getOptionalInput()}
-            {/* <button onClick={() => {this.setState({input:true});this.props.onClickEdit(this.props.commentId, this.state.content)}}>edit</button> */}
-            {this.compareUsername()}
+                <div>
+                    
+                creator: {this.props.username},
+                creationTime: {this.props.creationTime}
+                </div>
+
+                <div>
+                content: {this.props.content}
+                </div>
+
+                {/* <input type="text" value={this.state.content} onChange={e => this.setState({content: e.target.value})}></input> */}
+                {this.getOptionalInput()}
+                {/* <button onClick={() => {this.setState({input:true});this.props.onClickEdit(this.props.commentId, this.state.content)}}>edit</button> */}
+                {this.compareUsername()}
+                
             </div>
         )
     }
